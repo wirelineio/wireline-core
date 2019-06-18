1935cf1be5c4c4167bf92577965e7479f3986045
Martín Acosta <tinchoz49@gmail.com>
2019-06-18 09:36:12 -0300

    Added hyperswarm-network-memory and discovery-swarm-memory. (#20)
    * Added hyperswarm-network-memory.
    
    * Updated readme
    
    * Added duplex stream _read
    
    * Minor change
    
    * Ignore dist

---
+++ packages/discovery-swarm-memory/src/discovery-swarm.test.js
@@ -0,0 +1,35 @@
+//
+// Copyright 2019 Wireline, Inc.
+//
+
+import { promisify } from 'util';
+import hypercore from 'hypercore';
+import ram from 'random-access-memory';
+import DiscoverySwarm from './discovery-swarm';
+
+test('replicate using discovery-swarm and hypercore', async (done) => {
+  const localFeed = hypercore(ram, { valueEncoding: 'utf-8' });
+
+  await promisify(localFeed.ready.bind(localFeed))();
+
+  const replicaFeed = hypercore(ram, localFeed.key, { valueEncoding: 'utf-8' });
+  replicaFeed.on('append', () => {
+    replicaFeed.head((err, message) => {
+      expect(message).toBe('hello');
+      done();
+    });
+  });
+
+  const swarms = [
+    new DiscoverySwarm({
+      stream: () => localFeed.replicate({ live: true })
+    }),
+    new DiscoverySwarm({
+      stream: () => replicaFeed.replicate({ live: true })
+    })
+  ];
+
+  swarms.forEach(swarm => swarm.join('test'));
+
+  localFeed.append('hello');
+});
---
+++ packages/hyperswarm-network-memory/src/socket.js
@@ -0,0 +1,74 @@
+//
+// Copyright 2019 Wireline, Inc.
+//
+
+import { Duplex } from 'stream';
+
+/**
+ * Socket
+ *
+ * DuplexStream that simulate a network connection beetwen two endpoints.
+ *
+ * @extends {Duplex}
+ */
+class Socket extends Duplex {
+
+  /**
+   * constructor
+   *
+   * @returns {undefined}
+   */
+  constructor() {
+    super();
+
+    this._endpoint = null;
+  }
+
+  /**
+   * Set the socket endpoint where the local one is going to send the data.
+   *
+   * @param {Socker} socket
+   * @returns {undefined}
+   */
+  setEndpoint(socket) {
+    this._endpoint = socket;
+    if (socket._endpoint !== this) {
+      socket.setEndpoint(this);
+    }
+  }
+
+  /**
+   * Send data to one socket to another.
+   *
+   * @param {Buffer} buffer
+   * @returns {undefined}
+   */
+  send(buffer) {
+    if (this.destroyed) return;
+
+    setTimeout(() => {
+      if (this.destroyed) return;
+
+      this.push(buffer);
+    }, 1);
+  }
+
+  /**
+   * Private _write hook method required by the DuplexStream class.
+   *
+   * @param {Buffer} data
+   * @param {string} enc
+   * @param {function(err, data)} cb
+   * @returns {undefined}
+   */
+  _write(data, enc, cb) {
+    setTimeout(() => {
+      this._endpoint.send(data);
+      cb(null, data);
+    }, 1);
+  }
+
+  _read() {} // eslint-disable-line
+}
+
+export default Socket;
