fe735ab279843b25a470028ccc6889766eff3c7c
Martín Acosta <tinchoz49@gmail.com>
2019-06-28 15:14:45 -0300

    Fixed destroy in hyperswarm-network-memory.

--- packages/hyperswarm-network-memory/src/socket.js
+++ packages/hyperswarm-network-memory/src/socket.js
@@ -62,6 +62,8 @@ class Socket extends Duplex {
    * @returns {undefined}
    */
   _write(data, enc, cb) {
+    if (this.destroyed) return;
+
     setTimeout(() => {
       this._endpoint.send(data);
       cb(null, data);
@@ -69,6 +71,17 @@ class Socket extends Duplex {
   }
 
   _read() {} // eslint-disable-line
+
+  _destroy(err) {
+    if (err) {
+      this.emit('error', err);
+    }
+
+    if (!this._readableState.ended) this.push(null);
+    if (!this._writableState.finished) this.end();
+
+    this.emit('close');
+  }
 }
 
 export default Socket;
