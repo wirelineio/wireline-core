579df96e42e29fced4d83e0234a5f1fa930cc01f
Martín Acosta <tinchoz49@gmail.com>
2019-07-05 11:13:41 -0300

    Refactor handshake replicator (#52)
    * Moved handshake replicator handler to the new extension life-cycle.
    
    * Added jsdoc.

--- packages/megafeed2/src/megafeed/replicator.js
+++ packages/megafeed2/src/megafeed/replicator.js
@@ -42,13 +42,69 @@ export class Replicator extends EventEmitter {
   createExtension() {
     return new Extension(Replicator.extension, { timeout: this._options.timeout })
       .on('error', err => this.emit(err))
-      .setMessageHandler(this._extensionHandler.bind(this));
+      .setHandshakeHandler(this._handshakeHandler.bind(this))
+      .setMessageHandler(this._messageHandler.bind(this));
+  }
+
+  /**
+   * Discover topics from peer.
+   * @param protocol
+   * @returns {Promise<[{string}]>}
+   */
+  async getTopics(protocol) {
+    const extension = protocol.getExtension(Replicator.extension);
+    console.assert(extension);
+
+    // Ask peer for topics.
+    const { response: { topics } } = await extension.send({ type: 'get-topics' });
+
+    return topics;
+  }
+
+  /**
+   * Start replicating topics.
+   *
+   * @param {Protocol} protocol
+   * @returns {Promise<void>}
+   */
+  async _handshakeHandler(protocol) {
+    const extension = protocol.getExtension(Replicator.extension);
+    console.assert(extension);
+
+    const topics = await this.getTopics(protocol);
+
+    // NOTE: Does not wait to complete.
+    // Ask peer for topic feeds and replicate.
+    const { response: { feedKeysByTopic } } = await extension.send({ type: 'get-keys', topics });
+    feedKeysByTopic.forEach(async ({ topic, keys }) => {
+      await Promise.all(keys.map(async (key) => {
+        const path = `feed/${topic}/${key}`;
+        const feed = await this._feedStore.openFeed(path, { key: Buffer.from(key, 'hex'), metadata: { topic } });
+
+        // TODO(burdon): Test if already replicating?
+        // Share and replicate feeds over protocol stream.
+        protocol.stream.feed(key);
+
+        // Start replication.
+        feed.replicate({ live: true, stream: protocol.stream });
+
+        // TODO(burdon): Only add once.
+        // Propagate replication events.
+        feed.on('sync', () => {
+          this.emit('update', { topic, feed });
+        });
+      }));
+    });
   }
 
   /**
    * Handles key exchange requests.
+   *
+   * @param {Protocol} protocol
+   * @param {Object} context
+   * @param {Object} message
    */
-  async _extensionHandler(protocol, context, message) {
+  async _messageHandler(protocol, context, message) {
     // TODO(burdon): Check credentials. By topic?
     if (!context.user) {
       throw new ProtocolError(401);
@@ -108,52 +164,4 @@ export class Replicator extends EventEmitter {
     }
   }
 
-  /**
-   * Discover topics from peer.
-   * @param protocol
-   * @returns {Promise<[{string}]>}
-   */
-  async getTopics(protocol) {
-    const extension = protocol.getExtension(Replicator.extension);
-    console.assert(extension);
-
-    // Ask peer for topics.
-    const { response: { topics } } = await extension.send({ type: 'get-topics' });
-
-    return topics;
-  }
-
-  /**
-   * Start replicating topics.
-   * @param protocol
-   * @param topics
-   * @returns {Promise<void>}
-   */
-  async replicateTopics(protocol, topics) {
-    const extension = protocol.getExtension(Replicator.extension);
-    console.assert(extension);
-
-    // NOTE: Does not wait to complete.
-    // Ask peer for topic feeds and replicate.
-    const { response: { feedKeysByTopic } } = await extension.send({ type: 'get-keys', topics });
-    feedKeysByTopic.forEach(async ({ topic, keys }) => {
-      await Promise.all(keys.map(async (key) => {
-        const path = `feed/${topic}/${key}`;
-        const feed = await this._feedStore.openFeed(path, { key: Buffer.from(key, 'hex'), metadata: { topic } });
-
-        // TODO(burdon): Test if already replicating?
-        // Share and replicate feeds over protocol stream.
-        protocol.stream.feed(key);
-
-        // Start replication.
-        feed.replicate({ live: true, stream: protocol.stream });
-
-        // TODO(burdon): Only add once.
-        // Propagate replication events.
-        feed.on('sync', () => {
-          this.emit('update', { topic, feed });
-        });
-      }));
-    });
-  }
 }
