7182ae89cdb0d34589f5dc05b0ffdce693e9a246
Max Fierro <elmasse@gmail.com>
2019-07-25 14:13:09 -0300

    Remove unused package @wirelineio/party

--- packages/party/src/codec.js
+++
@@ -1,8 +0,0 @@
-const Codec = require('@wirelineio/codec-protobuf');
-
-const schema = require('./schema.json');
-
-const codec = new Codec({ verify: true });
-codec.loadFromJSON(schema);
-
-module.exports = codec;
